fcddf969773538b95f19f47d0619522b1db7b84c
彭驰 <pengchi@howzhi.com>
2018-04-09 20:56:23 +0800

    Init Joker project

---
+++ joker/src/main/java/me/chrispeng/joker/JokerApplication.java
@@ -0,0 +1,12 @@
+package me.chrispeng.joker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JokerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JokerApplication.class, args);
+	}
+}
