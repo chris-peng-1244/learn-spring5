08a413895ba7a67c62a57122d316b4c0f4c554b9
彭驰 <pengchi@howzhi.com>
2018-04-14 06:53:10 +0800

    Add joker file

--- joker/src/main/java/me/chrispeng/joker/JokerApplication.java
+++ joker/src/main/java/me/chrispeng/joker/JokerApplication.java
@@ -2,8 +2,10 @@ package me.chrispeng.joker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("classpath:chuck-config.xml")
 public class JokerApplication {
 
 	public static void main(String[] args) {
