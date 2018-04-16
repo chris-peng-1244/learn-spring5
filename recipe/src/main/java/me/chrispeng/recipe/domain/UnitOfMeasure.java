21d714a3504ef77376691b1668d73dada2edf4a3
彭驰 <pengchi@howzhi.com>
2018-04-16 11:25:09 +0800

    Add domains to project

---
+++ recipe/src/main/java/me/chrispeng/recipe/domain/UnitOfMeasure.java
@@ -0,0 +1,30 @@
+package me.chrispeng.recipe.domain;
+
+import org.hibernate.loader.plan.build.internal.LoadGraphLoadPlanBuildingStrategy;
+
+import javax.persistence.*;
+
+@Entity
+public class UnitOfMeasure {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String uom;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getUom() {
+		return uom;
+	}
+
+	public void setUom(String uom) {
+		this.uom = uom;
+	}
+}
